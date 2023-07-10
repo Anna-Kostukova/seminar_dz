3a257b204c1b19ad5baf7df02e8cfc150ce1fb9a
«Anna» <anna.kostukova@gmail.com>
2023-07-10 17:40:01 +0500

    доработать

---
+++ ДЗ2/Задача_10/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int GetRandomNumber()
+{
+int number = new Random().Next(100, 1000);
+return number;
+}
+
+int GetLastDigit(int number1) 
+{
+    number1 = number1 / 10;
+    return number1 % 10;
+}
+
+void PrintNumber(int num1) // невозвращаемый метод, выводим на печать
+{
+    Console.Write(num1 + " -> " + " ");
+}
+
+int num = GetRandomNumber();
+PrintNumber(num);
+int lastdigit = GetLastDigit(num);
+PrintNumber(lastdigit);
+
