56a1451bd44c7573fe4f9fb216c31fd15b84df23
«Anna» <anna.kostukova@gmail.com>
2023-07-10 22:45:01 +0500

    готово

--- ДЗ2/Задача_10/Program.cs
+++ ДЗ2/Задача_10/Program.cs
@@ -6,23 +6,23 @@
 
 int GetRandomNumber()
 {
-int number = new Random().Next(100, 1000);
+int number = new Random().Next(100, 1000); // метод выбора случайных чисел 0т 100 до 999
 return number;
 }
 
-int GetLastDigit(int number1) 
+int GetLastDigit(int number1) // метод вычисления второй цифры
 {
     number1 = number1 / 10;
     return number1 % 10;
 }
 
-void PrintNumber(int num1) // невозвращаемый метод, выводим на печать
+void PrintNumber(int num1) // метод вывода текста на консоль
 {
-    Console.Write($"Вторая цифра числа: {num1}" + " ");
+    Console.WriteLine($"Вторая цифра этого числа: {num1}" + " ");
 }
 
-int num = GetRandomNumber();
-PrintNumber(num);
-int lastdigit = GetLastDigit(num);
-PrintNumber(lastdigit);
+int num = GetRandomNumber(); // выбрано число
+Console.WriteLine("Число: " +  num); // выводим на консоль рандомное число
+int lastdigit = GetLastDigit(num); // результат вычисления второго числа
+PrintNumber(lastdigit); // выводим на консоль вторую цифру
 
