0238e14f31352694679c34a6c74fac5509428481
«Anna» <anna.kostukova@gmail.com>
2023-07-16 21:15:36 +0500

    задача_27

---
+++ ДЗ4/Задача_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int SummAllNumber( int number)
+{
+    int num = 0;
+    while (number > 0)
+    {
+        num += number % 10;
+        number = number / 10;
+    }
+    return num;
+}
+
+int number = Promt("Введите любое целое число: ");
+Console.Write($"Сумма чисел в цифре {number} равна {SummAllNumber(number)}");
